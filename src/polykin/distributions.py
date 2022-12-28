d194391be860853472f3ecc413e1aa83ed19ac98
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2022-12-28 13:54:25 +0100

    add show, plot

--- src/polykin/distributions.py
+++ src/polykin/distributions.py
@@ -2,14 +2,19 @@
 
 from abc import ABC, abstractmethod
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class Distribution(ABC):
     """Abstract class for all chain-length distributions."""
 
-    def __init__(self, DPn: int = 100, molar_mass: float = 100.0):
+    def __init__(self, DPn: int = 100, molar_mass: float = 100.0, name: str = ""):
         self.DPn = DPn
         self.molar_mass = molar_mass
+        if name != "":
+            self.name = name
+        else:
+            self.name = self.__class__.__name__
 
     def __call__(self, x, dist: str = "mass", unit_x: str = "chain_length"):
 
@@ -23,28 +28,76 @@ class Distribution(ABC):
 
         # Select distribution
         if dist == "number":
-            w = self.dist_number(x)
+            w = self._dist_number(x)
         elif dist == "mass":
-            w = self.dist_mass(x)
+            w = self._dist_mass(x)
         elif dist == "gpc":
-            w = self.dist_gpc(x)
+            w = self._dist_gpc(x)
         else:
             raise ValueError
 
         return w
 
-    def dist_number(self, length):
+    def _dist_number(self, length):
         return self._pmf(self._list_to_array(length))
 
-    def dist_mass(self, length):
-        return self.dist_number(length) * length
+    def _dist_mass(self, length):
+        return self._dist_number(length) * length / self._DPn
 
-    def dist_gpc(self, length):
-        return self.dist_number(length) * length**2
+    def _dist_gpc(self, length):
+        return self._dist_number(length) * length**2 / self._moment(2)
+
+    @property
+    def DPn(self):
+        """Number-average degree of polymerization, $DP_n$."""
+        return self._DPn
+
+    @DPn.setter
+    def DPn(self, DPn: int = 100):
+        """Set average degree of polymerization."""
+        self._DPn = DPn
+
+    @property
+    def DPw(self):
+        """Mass-average degree of polymerization, $DP_w$."""
+        return self._moment(2) / self._moment(1)
+
+    @property
+    def DPz(self):
+        """z-average degree of polymerization, $DP_z$."""
+        return self._moment(3) / self._moment(2)
+
+    @property
+    def PDI(self):
+        """Polydispersity index, $DP_w/DP_n$."""
+        return self.DPw / self.DPn
+
+    @property
+    def Mn(self):
+        """Number-average molar mass, $M_n$."""
+        return self.molar_mass * self.DPn
+
+    @property
+    def Mw(self):
+        """Weight-average molar mass, $M_w$."""
+        return self.molar_mass * self.DPw
+
+    @property
+    def Mz(self):
+        """z-average molar mass, $M_z$."""
+        return self.molar_mass * self.DPz
 
     @abstractmethod
     def _pmf(self, length):
-        return 0
+        return 0.0
+
+    @abstractmethod
+    def _cdf(self, length):
+        return 0.0
+
+    @abstractmethod
+    def _moment(self, order: int):
+        return 0.0
 
     @staticmethod
     def _list_to_array(length):
@@ -52,16 +105,68 @@ class Distribution(ABC):
             length = np.asarray(length)
         return length
 
+    @property
+    def show(self):
+        """Show key properties of the chain-length distribution."""
+        print(f"DPn: {self.DPn:.1f}")
+        print(f"DPw: {self.DPw:.1f}")
+        print(f"DPz: {self.DPw:.1f}")
+        print(f"PDI: {self.PDI:.2f}")
+        print(f"Mn:  {self.Mn:,.0f}")
+        print(f"Mw:  {self.Mw:,.0f}")
+        print(f"Mz:  {self.Mz:,.0f}")
+
+    def plot(self, dist: str = "mass", unit_x: str = "chain_length", ax=None):
+        """Plot the chain-length distribution."""
+
+        # Create axis if none is provided
+        if ax is None:
+            fig, ax = plt.subplots(1, 1)
+            fig.suptitle(f"Distribution: {self.name}")
+            self.fig = fig
+
+        # Plot distribution
+        x = np.linspace(1, 10 * self._DPn, 100)
+        y = self(x, dist=dist, unit_x="chain_length")
+        if unit_x == "molar_mass":
+            x = x * self.molar_mass
+            label_x = "Molar mass"
+        elif unit_x == "chain_length":
+            label_x = "Chain length"
+        else:
+            raise ValueError
+        ax.plot(x, y, label=dist)
+        ax.legend(bbox_to_anchor=(1.05, 1.0), loc="upper left")
+        ax.grid(True)
+        ax.set_xlabel(label_x)
+        ax.set_ylabel("Relative abundance")
+
+        return ax
+
 
 class Flory(Distribution):
-    """Flory-Schulz (aka most-probable) chain-length distribution.
-    https://en.wikipedia.org/wiki/Flory%E2%80%93Schulz_distribution
-    """
+    """Flory-Schulz (aka most-probable) chain-length distribution."""
 
     def _pmf(self, i):
-        a = 2.0 / (self.DPn + 1)
-        w = a**2 * i * (1 - a) ** (i - 1)
-        return w
+        a = 1 - 1 / self.DPn
+        result = (1 - a) * a ** (i - 1)
+        return result
+
+    def _cdf(self, i):
+        return 0.0
+
+    def _moment(self, order: int):
+        if order == 0:
+            result = 1
+        elif order == 1:
+            result = self.DPn
+        elif order == 2:
+            result = self.DPn * (2 * self.DPn - 1)
+        elif order == 3:
+            result = self.DPn * (1 + 6 * self.DPn * (self.DPn - 1))
+        else:
+            raise ValueError
+        return result
 
 
 class Poisson(Distribution):
@@ -69,3 +174,10 @@ class Poisson(Distribution):
         a = 2.0 / (self.DPn + 1)
         w = a**2 * i * (1 - a) ** (i - 1)
         return w
+
+
+# %%
+
+d = Flory(142)
+d.show
+d.plot(dist="mass")
--- tests/test_distributions.py
+++ tests/test_distributions.py
@@ -1,7 +1,20 @@
 from polykin.distributions import Flory, Poisson
+import numpy as np
 
 
 def test_init():
-    dist = {"flory": Flory(), "poisson": Poisson()}
-    for item in dist.values():
-        assert item(100) > 0
+    distributions = {"flory": Flory(), "poisson": Poisson()}
+    for name, item in distributions.items():
+        for dist in ["number", "mass", "gpc"]:
+            w = item(100, dist=dist)
+            print(f"{name}, {dist}: {w}")
+            assert w > 0
+
+
+def test_Flory():
+    N = 100
+    d = Flory(N)
+    assert d.DPn == N
+    assert np.isclose(d.DPw, 2 * N, atol=1)
+    assert np.isclose(d.PDI, 2, rtol=1e-2)
+    d.show()
