940225ccbdb134d1929485e4ebb8170bb6cd3f5c
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2023-01-08 18:47:31 +0100

    change import path

--- src/polykin/distributions/__init__.py
+++ src/polykin/distributions/__init__.py
@@ -1,3 +1,3 @@
 """A polymerization kinetics framework"""
 
-from .singledistributions import Flory, Poisson, LogNormal, SchulzZimm
+from polykin.distributions.singledistributions import Flory, Poisson, LogNormal, SchulzZimm
