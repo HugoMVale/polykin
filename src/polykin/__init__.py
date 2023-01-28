7ffddecc84a67a0359f5e03b4d62bae30e09a1e6
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2023-01-28 16:01:17 +0100

    doc + ExperimentalDistribution

--- src/polykin/__init__.py
+++ src/polykin/__init__.py
@@ -1,4 +1,16 @@
-"""A polymerization kinetics framework"""
+"""
+# PolyKin: A polymerization kinetics library for Python.
+
+Documentation is available in the docstrings and online at https://hugomvale.github.io/polykin.
+
+## Subpackages
+
+Using any of these subpackages requires an explicit import. For example,
+`import polykin.distributions`.
+
+* distributions = chain-length distributions
+
+"""
 
 from pathlib import Path
 
--- src/polykin/distributions/__init__.py
+++ src/polykin/distributions/__init__.py
@@ -1,4 +1,9 @@
-"""A polymerization kinetics framework"""
+"""
+PolyKin `distributions` provides classes to create, visualize, fit, combine,
+integrate, etc. theoretical and experimental chain-length distributions.
 
-from polykin.distributions.individualdistributions import \
+"""
+
+from polykin.distributions.analytical import \
     Flory, Poisson, LogNormal, SchulzZimm
+from polykin.distributions.experimental import ExperimentalDistribution
