28255a4cf82712f5c81498f90ccb5e26fd5da4bc
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2023-01-29 19:12:33 +0100

    init copolymerization

---
+++ src/polykin/copolymerization/__init__.py
@@ -0,0 +1,7 @@
+"""
+PolyKin `copolymerization` provides classes to create, visualize, fit, combine,
+integrate, etc. theoretical and experimental chain-length distributions.
+
+"""
+
+# from polykin.copolymerization import
