8c271f1cf91814f58a5f0fa795e725c053cc1f60
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2023-04-04 21:46:52 +0200

    remove add paths

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,9 +1,9 @@
-import sys
-import os
+# import sys
+# import os
 
-here = os.path.dirname(os.path.abspath(__file__))
-path_list = ["../src/", "../src/polykin", "../src/polykin/distributions"]
+# here = os.path.dirname(os.path.abspath(__file__))
+# path_list = ["../src/", "../src/polykin", "../src/polykin/distributions"]
 
-for path in path_list:
-    path_to_add = os.path.join(here, path)
-    sys.path.append(path_to_add)
+# for path in path_list:
+#     path_to_add = os.path.join(here, path)
+#     sys.path.append(path_to_add)
