a9efb7e5cf0b07dbf1678cc0c67ff815e65fb552
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2023-07-17 22:28:33 +0200

    add test PropagationHalfLength

--- tests/test_coefficients.py
+++ tests/test_coefficients.py
@@ -1,6 +1,7 @@
-from polykin.coefficients import Arrhenius, Eyring, CompositeModelTermination,\
+from polykin.coefficients import Arrhenius, Eyring, \
+    TerminationCompositeModel, PropagationHalfLength,\
     DIPPR100, DIPPR101, DIPPR105
-from polykin.utils import RangeWarning, RangeError
+from polykin.utils import RangeError
 
 import pytest
 import numpy as np
@@ -98,6 +99,7 @@ def test_product_Arrhenius_Arrhenius_scalar(capsys):
     out, _ = capsys.readouterr()
     assert (out.lower().startswith('warning'))
 
+
 def test_division_Arrhenius_Arrhenius_scalar(capsys):
     k1 = Arrhenius(1e2, 1e4, T0=340, Tmin=300, Tmax=380, name='k1')
     k2 = Arrhenius(2e2, 5e3, T0=360, Tmin=320, Tmax=400, name='k2')
@@ -113,6 +115,7 @@ def test_division_Arrhenius_Arrhenius_scalar(capsys):
     _ = k3(310, 'K')
     out, _ = capsys.readouterr()
     assert (out.lower().startswith('warning'))
+
 
 def test_product_Arrhenius_Arrhenius_array(capsys):
     k1 = Arrhenius([1e2, 2e3], [2e4, 1e4], T0=[340, 341],
@@ -132,6 +135,7 @@ def test_product_Arrhenius_Arrhenius_array(capsys):
     out, _ = capsys.readouterr()
     assert (out.lower().startswith('warning'))
 
+
 def test_division_Arrhenius_Arrhenius_array(capsys):
     k1 = Arrhenius([1e2, 2e3], [2e4, 1e4], T0=[340, 341],
                    Tmin=[300, 301], Tmax=[380, 381], name='k1')
@@ -150,25 +154,39 @@ def test_division_Arrhenius_Arrhenius_array(capsys):
     out, _ = capsys.readouterr()
     assert (out.lower().startswith('warning'))
 
+
 def test_evaluation_Eyring():
     DSa = [1e2, 0]
     DHa = [0, 73.5e3]
     kappa = [0.5, 1]
     Tref = 300.
     k = Eyring(DSa, DHa, kappa, name='test')
-    assert np.all((np.isclose(k(2*Tref, 'K')[0]/k(Tref, 'K')[0], 2)))
-    assert (np.isclose(k(Tref, 'K')[-1], 1, rtol=0.1))
+    assert np.all(
+        np.isclose(k(2*Tref, 'K')[0]/k(Tref, 'K')[0], 2))  # type: ignore
+    assert (np.isclose(k(Tref, 'K')[-1], 1, rtol=0.1))  # type: ignore
 
 
-def test_evaluation_TerminationCompostiteModel():
+def test_evaluation_TerminationCompositeModel():
     T0 = 298
     icrit = 50
     aS = 0.5
     aL = 0.2
     kt11 = Arrhenius(1, 2000, T0, name='kt11')
-    kt = CompositeModelTermination(kt11, icrit, aS, aL, 'kt')
+    kt = TerminationCompositeModel(kt11, icrit, aS, aL, 'kt')
     assert (np.isclose(kt.eval(T0, icrit, icrit), kt11.eval(T0)/icrit**aS))
-    assert len(kt.eval(T0, np.arange(1, 1000, 1), np.arange(1, 1000, 1)))
+    assert len(kt.eval(T0, np.arange(1, 1000, 1),
+               np.arange(1, 1000, 1)))  # type: ignore
+
+
+def test_evaluation_PropagationHalfLength():
+    T0 = 298
+    ihalf = 2
+    C = 11
+    kp = Arrhenius(1e3, 2000, T0=298., name='kp(inf)')
+    kpi = PropagationHalfLength(kp, C, ihalf, name='kp(i)')
+    assert (np.isclose(kpi.eval(T0, 1)/kp.eval(T0), C))
+    assert (np.isclose(kpi.eval(T0, ihalf+1)/kp.eval(T0), (C+1)/2))
+    assert len(kpi.eval(T0, np.arange(1, 101)))  # type: ignore
 
 
 def test_DIPPR_100():
