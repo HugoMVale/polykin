28e754c0aabc3230e7b6d22f24a5849de5aef3f2
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2023-09-17 22:04:43 +0200

    add import

--- src/polykin/coefficients/__init__.py
+++ src/polykin/coefficients/__init__.py
@@ -9,3 +9,4 @@ from polykin.coefficients.kinetics import Arrhenius, Eyring
 from polykin.coefficients.dippr import DIPPR100, DIPPR101, DIPPR105
 from polykin.coefficients.cld import TerminationCompositeModel, \
     PropagationHalfLength
+from polykin.coefficients.baseclasses import plotcoeffs
