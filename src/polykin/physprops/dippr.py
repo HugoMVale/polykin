309948bd016439db2b3af6889684a9768fa613a9
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2023-10-07 11:41:52 +0200

    add functions

--- src/polykin/physprops/dippr.py
+++ src/polykin/physprops/dippr.py
@@ -7,8 +7,12 @@ from polykin.physprops.propertyequation import PropertyEquationT
 
 import numpy as np
 
-__all__ = ['DIPPR100', 'DIPPR101', 'DIPPR102', 'DIPPR104', 'DIPPR105',
-           'DIPPR106']
+__all__ = ['DIPPR100', 'dippr100',
+           'DIPPR101', 'dippr101',
+           'DIPPR102', 'dippr102',
+           'DIPPR104', 'dippr104',
+           'DIPPR105', 'dippr105',
+           'DIPPR106', 'dippr106']
 
 
 class DIPPR(PropertyEquationT):
@@ -109,6 +113,8 @@ class DIPPRP5(DIPPR):
             f"Tmax [K]:  {self.Tmax}"
         )
 
+# %% DIPPR 100
+
 
 class DIPPR100(DIPPRP5):
     r"""[DIPPR](https://de.wikipedia.org/wiki/DIPPR-Gleichungen)-100 equation.
@@ -176,12 +182,48 @@ class DIPPR100(DIPPRP5):
         FloatOrArray
             Property value, $Y$.
         """
-        A = self.A
-        B = self.B
-        C = self.C
-        D = self.D
-        E = self.E
-        return A + B * T + C * T**2 + D * T**3 + E * T**4
+        return dippr100(T, self.A, self.B, self.C, self.D, self.E)
+
+
+def dippr100(T: FloatOrArray,
+             A: float = 0.,
+             B: float = 0.,
+             C: float = 0.,
+             D: float = 0.,
+             E: float = 0.,
+             ) -> FloatOrArray:
+    r"""[DIPPR](https://de.wikipedia.org/wiki/DIPPR-Gleichungen)-100 equation.
+
+    This equation implements the following temperature dependence:
+
+    $$ Y = A + B T + C T^2 + D T^3 + E T^4 $$
+
+    where $A$ to $E$ are constant parameters and $T$ is the absolute
+    temperature.
+
+    Parameters
+    ----------
+    T : FloatOrArray
+        Temperature.
+        Unit = K.
+    A : float
+        Parameter of equation.
+    B : float
+        Parameter of equation.
+    C : float
+        Parameter of equation.
+    D : float
+        Parameter of equation.
+    E : float
+        Parameter of equation.
+
+    Returns
+    -------
+    FloatOrArray
+        Property value, $Y$.
+    """
+    return A + B * T + C * T**2 + D * T**3 + E * T**4
+# %% DIPPR 101
 
 
 class DIPPR101(DIPPRP5):
@@ -250,12 +292,49 @@ class DIPPR101(DIPPRP5):
         FloatOrArray
             Property value, $Y$.
         """
-        A = self.A
-        B = self.B
-        C = self.C
-        D = self.D
-        E = self.E
-        return np.exp(A + B / T + C * np.log(T) + D * T**E)
+        return dippr101(T, self.A, self.B, self.C, self.D, self.E)
+
+
+def dippr101(T: FloatOrArray,
+             A: float,
+             B: float,
+             C: float = 0.,
+             D: float = 0.,
+             E: float = 0.,
+             ) -> FloatOrArray:
+    r"""[DIPPR](https://de.wikipedia.org/wiki/DIPPR-Gleichungen)-101 equation.
+
+    This equation implements the following temperature dependence:
+
+    $$ Y = \exp{\left(A + B / T + C \ln(T) + D T^E\right)} $$
+
+    where $A$ to $E$ are constant parameters and $T$ is the absolute
+    temperature.
+
+    Parameters
+    ----------
+    T : FloatOrArray
+        Temperature.
+        Unit = K.
+    A : float
+        Parameter of equation.
+    B : float
+        Parameter of equation.
+    C : float
+        Parameter of equation.
+    D : float
+        Parameter of equation.
+    E : float
+        Parameter of equation.
+
+    Returns
+    -------
+    FloatOrArray
+        Property value, $Y$.
+    """
+    return np.exp(A + B / T + C * np.log(T) + D * T**E)
+
+# %% DIPPR 102
 
 
 class DIPPR102(DIPPRP4):
@@ -321,11 +400,46 @@ class DIPPR102(DIPPRP4):
         FloatOrArray
             Property value, $Y$.
         """
-        A = self.A
-        B = self.B
-        C = self.C
-        D = self.D
-        return (A * T**B) / (1 + C/T + D/T**2)
+        return dippr102(T, self.A, self.B, self.C, self.D)
+
+
+def dippr102(T: FloatOrArray,
+             A: float,
+             B: float,
+             C: float = 0.,
+             D: float = 0.,
+             ) -> FloatOrArray:
+    r"""[DIPPR](https://de.wikipedia.org/wiki/DIPPR-Gleichungen)-102 equation.
+
+    This equation implements the following temperature dependence:
+
+    $$ Y = \frac{A T^B}{ 1 + C/T + D/T^2} $$
+
+    where $A$ to $D$ are constant parameters and $T$ is the absolute
+    temperature.
+
+    Parameters
+    ----------
+    T : FloatOrArray
+        Temperature.
+        Unit = K.
+    A : float
+        Parameter of equation.
+    B : float
+        Parameter of equation.
+    C : float
+        Parameter of equation.
+    D : float
+        Parameter of equation.
+
+    Returns
+    -------
+    FloatOrArray
+        Property value, $Y$.
+    """
+    return (A * T**B) / (1 + C/T + D/T**2)
+
+# %% DIPPR 104
 
 
 class DIPPR104(DIPPRP5):
@@ -394,12 +508,49 @@ class DIPPR104(DIPPRP5):
         FloatOrArray
             Property value, $Y$.
         """
-        A = self.A
-        B = self.B
-        C = self.C
-        D = self.D
-        E = self.E
-        return A + B/T + C/T**3 + D/T**8 + E/T**9
+        return dippr104(T, self.A, self.B, self.C, self.D, self.E)
+
+
+def dippr104(T: FloatOrArray,
+             A: float,
+             B: float,
+             C: float = 0.,
+             D: float = 0.,
+             E: float = 0.,
+             ) -> FloatOrArray:
+    r"""[DIPPR](https://de.wikipedia.org/wiki/DIPPR-Gleichungen)-104 equation.
+
+    This equation implements the following temperature dependence:
+
+    $$ Y = A + B/T + C/T^3 + D/T^8 + E/T^9 $$
+
+    where $A$ to $E$ are constant parameters and $T$ is the absolute
+    temperature.
+
+    Parameters
+    ----------
+    T : FloatOrArray
+        Temperature.
+        Unit = K.
+    A : float
+        Parameter of equation.
+    B : float
+        Parameter of equation.
+    C : float
+        Parameter of equation.
+    D : float
+        Parameter of equation.
+    E : float
+        Parameter of equation.
+
+    Returns
+    -------
+    FloatOrArray
+        Property value, $Y$.
+    """
+    return A + B/T + C/T**3 + D/T**8 + E/T**9
+
+# %% DIPPR 105
 
 
 class DIPPR105(DIPPRP4):
@@ -465,11 +616,46 @@ class DIPPR105(DIPPRP4):
         FloatOrArray
             Property value, $Y$.
         """
-        A = self.A
-        B = self.B
-        C = self.C
-        D = self.D
-        return A / B**(1 + (1 - T / C)**D)
+        return dippr105(T, self.A, self.B, self.C, self.D)
+
+
+def dippr105(T: FloatOrArray,
+             A: float,
+             B: float,
+             C: float,
+             D: float,
+             ) -> FloatOrArray:
+    r"""[DIPPR](https://de.wikipedia.org/wiki/DIPPR-Gleichungen)-105 equation.
+
+    This equation implements the following temperature dependence:
+
+    $$ Y = \frac{A}{B^{ \left( 1 + (1 - T / C)^D \right) }} $$
+
+    where $A$ to $D$ are constant parameters and $T$ is the absolute
+    temperature.
+
+    Parameters
+    ----------
+    T : FloatOrArray
+        Temperature.
+        Unit = K.
+    A : float
+        Parameter of equation.
+    B : float
+        Parameter of equation.
+    C : float
+        Parameter of equation.
+    D : float
+        Parameter of equation.
+
+    Returns
+    -------
+    FloatOrArray
+        Property value, $Y$.
+    """
+    return A / B**(1 + (1 - T / C)**D)
+
+# %% DIPPR 106
 
 
 class DIPPR106(DIPPR):
@@ -575,10 +761,50 @@ class DIPPR106(DIPPR):
         FloatOrArray
             Property value, $Y$.
         """
-        A = self.A
-        B = self.B
-        C = self.C
-        D = self.D
-        E = self.E
-        Tr = T/self.Tc
-        return A * (1 - Tr) ** (B + Tr*(C + Tr*(D + E*Tr)))
+        return dippr106(T, self.Tc, self.A, self.B, self.C, self.D, self.E)
+
+
+def dippr106(T: FloatOrArray,
+             Tc: float,
+             A: float,
+             B: float,
+             C: float = 0.,
+             D: float = 0.,
+             E: float = 0.
+             ) -> FloatOrArray:
+    r"""[DIPPR](https://de.wikipedia.org/wiki/DIPPR-Gleichungen)-106 equation.
+
+    This equation implements the following temperature dependence:
+
+    $$ Y = A (1 - T_r)^{B + C T_r + D T_r^2 + E T_r^3} $$
+
+    where $A$ to $E$ are constant parameters, $T$ is the absolute temperature,
+    $T_c$ is the critical temperature and $T_r = T/T_c$ is the reduced
+    temperature.
+
+    Parameters
+    ----------
+    T : FloatOrArray
+        Temperature.
+        Unit = K.
+    Tc : float
+        Critical temperature.
+        Unit = K.
+    A : float
+        Parameter of equation.
+    B : float
+        Parameter of equation.
+    C : float
+        Parameter of equation.
+    D : float
+        Parameter of equation.
+    E : float
+        Parameter of equation.
+
+    Returns
+    -------
+    FloatOrArray
+        Property value, $Y$.
+    """
+    Tr = T/Tc
+    return A * (1 - Tr) ** (B + Tr*(C + Tr*(D + E*Tr)))
