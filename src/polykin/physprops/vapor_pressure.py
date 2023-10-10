2d4718961c2fd153713ff220cdd82c9751ab6af4
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2023-10-10 22:22:25 +0200

    add Wagner

--- src/polykin/physprops/vapor_pressure.py
+++ src/polykin/physprops/vapor_pressure.py
@@ -7,18 +7,20 @@ from polykin.physprops.property_equation import PropertyEquationT
 
 import numpy as np
 
-__all__ = ['Antoine', 'antoine']
+__all__ = ['Antoine', 'Wagner']
 
 
 class Antoine(PropertyEquationT):
-    r"""[Antoine](https://en.wikipedia.org/wiki/Antoine_equation) equation.
+    r"""[Antoine](https://en.wikipedia.org/wiki/Antoine_equation) equation for
+    vapor pressure.
 
     This equation implements the following temperature dependence:
 
     $$ \log_{base} P^* = A - \frac{B}{T + C} $$
 
-    where $A$, $B$ and $C$ are constant parameters, $T$ is the temperature,
-    and $P^*$ is the vapor pressure.
+    where $A$, $B$ and $C$ are component-specific constants, $P^*$ is the vapor
+    pressure and $T$ is the temperature. When $C=0$, this equation reverts to
+    the Clapeyron equation.
 
     !!! note
         There is no consensus on the value of $base$, the unit of temperature,
@@ -28,8 +30,8 @@ class Antoine(PropertyEquationT):
 
     !!! hint
         The Antoine equation is limited in terms of temperature range. Wider
-        ranges can be achieved with
-        [DIPPR101](DIPPR.md).
+        ranges can be achieved with [DIPPR101](DIPPR.md) or
+        [Wagner](Wagner.md).
 
     Parameters
     ----------
@@ -57,15 +59,12 @@ class Antoine(PropertyEquationT):
         Name.
     """
 
-    A: float
-    B: float
-    C: float
-    base: float
+    _params = (('A', 'B', 'C'), ('base',))
 
     def __init__(self,
                  A: float,
                  B: float,
-                 C: float,
+                 C: float = 0.,
                  base: float = 10.,
                  Tmin: float = 0.0,
                  Tmax: float = np.inf,
@@ -79,22 +78,7 @@ class Antoine(PropertyEquationT):
         self.B = B
         self.C = C
         self.base = base
-        self.Trange = (Tmin, Tmax)
-        self.unit = unit
-        self.symbol = symbol
-        self.name = name
-
-    def __repr__(self) -> str:
-        return (
-            f"name:        {self.name}\n"
-            f"symbol:      {self.symbol}\n"
-            f"unit:        {self.unit}\n"
-            f"A:           {self.A}\n"
-            f"B:           {self.B}\n"
-            f"C:           {self.C}\n"
-            f"base:        {self.base}\n"
-            f"Trange [K]:  {self.Trange}"
-        )
+        super().__init__((Tmin, Tmax), unit, symbol, name)
 
     def eval(self, T: FloatOrArray) -> FloatOrArray:
         """Evaluate property equation at given SI conditions, without unit
@@ -111,54 +95,116 @@ class Antoine(PropertyEquationT):
         FloatOrArray
             Vapor pressure, $P^*$.
         """
-        return antoine(T, self.A, self.B, self.C, self.base)
+        A = self.A
+        B = self.B
+        C = self.C
+        base = self.base
+        return base**(A - B/(T + C))
 
 
-def antoine(T: FloatOrArray,
-            A: float,
-            B: float,
-            C: float,
-            base=10.
-            ) -> FloatOrArray:
-    r"""[Antoine](https://en.wikipedia.org/wiki/Antoine_equation) equation.
+# %% Wagner
+
+
+class Wagner(PropertyEquationT):
+
+    r"""[Wagner](https://de.wikipedia.org/wiki/Wagner-Gleichung) equation for
+    vapor pressure.
 
     This equation implements the following temperature dependence:
 
-    $$ \log_{base} P^* = A - \frac{B}{T + C} $$
+    $$ \ln(P^*/P_c) = \frac{a\tau + b\tau^{1.5} + c\tau^{2.5} + d\tau^5}{T_r}$$
 
-    where $A$, $B$ and $C$ are constant parameters, $T$ is the temperature,
-    and $P^*$ is the vapor pressure.
+    with:
+
+    $$ \tau = 1 - T_r$$
+
+    where $a$ to $d$ are component-specific constants, $P^*$ is the vapor
+    pressure, $P_c$ is the critical pressure, $T$ is the absolute temperature,
+    $T_c$ is the critical temperature, and $T_r=T/T_c$ is the reduced
+    temperature.
 
     !!! note
-        There is no consensus on the value of $base$, the unit of temperature,
-        or the unit of pressure. The function is flexible enough to accomodate
-        most cases, but care should be taken to ensure the parameters match the
-        intended use.
 
-    !!! hint
-        The Antoine equation is limited in terms of temperature range. Wider
-        ranges can be achieved with
-        [DIPPR101](DIPPR.md).
+        There are several versions of the Wagner equation with different
+        exponents. This is the so-called 25 version also used in the
+        [ThermoData Engine](https://trc.nist.gov/tde.html).
 
     Parameters
     ----------
-    T : FloatOrArray
-        Temperature.
-        Unit = (K, °C).
-    A : float
-        Parameter of equation.
-    B : float
-        Parameter of equation.
+    Tc : float
+        Critical temperature.
         Unit = K.
-    C : float
+    Pc : float
+        Critical pressure.
+        Unit = Any.
+    a : float
         Parameter of equation.
-        Unit = (K, °C).
-    base : float
-        Base of logarithm, usually equal to $10$ or $e$.
-
-    Returns
-    -------
-    FloatOrArray
-        Vapor pressure, $P^*$.
+    b : float
+        Parameter of equation.
+    c : float
+        Parameter of equation.
+    d : float
+        Parameter of equation.
+    Tmin : float
+        Lower temperature bound.
+        Unit = K.
+    Tmax : float
+        Upper temperature bound.
+        Unit = K.
+    unit : str
+        Unit of vapor pressure.
+    symbol : str
+        Symbol of vapor pressure.
+    name : str
+        Name.
     """
-    return base**(A - B/(T + C))
+
+    _params = (('a', 'b', 'c', 'd', 'Pc'), ('Tc',))
+
+    def __init__(self,
+                 Pc: float,
+                 Tc: float,
+                 a: float,
+                 b: float,
+                 c: float,
+                 d: float,
+                 Tmin: float = 0.0,
+                 Tmax: float = np.inf,
+                 unit: str = 'Pa',
+                 symbol: str = 'P^*',
+                 name: str = ''
+                 ) -> None:
+        """Construct `Wagner` with the given parameters."""
+
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
+        self.Pc = Pc
+        self.Tc = Tc
+        super().__init__((Tmin, Tmax), unit, symbol, name)
+
+    def eval(self, T: FloatOrArray) -> FloatOrArray:
+        """Evaluate property equation at given SI conditions, without unit
+        conversions or checks.
+
+        Parameters
+        ----------
+        T : FloatOrArray
+            Temperature.
+            Unit = K.
+
+        Returns
+        -------
+        FloatOrArray
+            Vapor pressure, $P^*$.
+        """
+        Tc = self.Tc
+        Pc = self.Pc
+        a = self.a
+        b = self.b
+        c = self.c
+        d = self.d
+        Tr = T/Tc
+        t = 1 - Tr
+        return Pc*np.exp((a*t + b*t**1.5 + c*t**2.5 + d*t**5)/Tr)
--- tests/physprops/test_vapor_pressure.py
+++ tests/physprops/test_vapor_pressure.py
@@ -2,7 +2,7 @@
 #
 # Copyright Hugo Vale 2023
 
-from polykin.physprops import Antoine
+from polykin.physprops import Antoine, Wagner
 
 
 # import pytest
@@ -15,4 +15,16 @@ def test_Antoine():
     """
     p = Antoine(A=4.6543, B=1435.264, C=-64.848,
                 Tmin=255.9, Tmax=373., unit='bar')
+    assert np.isclose(p(100., 'C'), 1.01325, rtol=2e-2)
+
+
+def test_Wagner():
+    """Pvap of water
+    Parameters: doi: 10.5541/ijot.372148
+    """
+    p = Wagner(Tc=647.096,
+               Pc=220.64,  # bar
+               a=-7.861942, b=1.879246, c=-2.266807,
+               d=-2.128615,
+               Tmin=273., Tmax=647., unit='bar')
     assert np.isclose(p(100., 'C'), 1.01325, rtol=2e-2)
