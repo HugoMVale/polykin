ddbcf078baaa3eb263b8aab7f9ad1f8b8c4bb32c
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2023-12-03 21:05:47 +0100

    extend with tuples

--- src/polykin/types.py
+++ src/polykin/types.py
@@ -7,17 +7,17 @@ from typing import Union, Any
 from nptyping import NDArray, Shape, Int32, Float64
 
 IntArray = NDArray[Any, Int32]
-IntArrayLike = Union[list[int], IntArray]
+IntArrayLike = Union[list[int], tuple[int, ...], IntArray]
 IntOrArray = Union[int, IntArray]
 IntOrArrayLike = Union[int, IntArrayLike]
 
 FloatArray = NDArray[Any, Float64]
-FloatArrayLike = Union[list[float], FloatArray]
+FloatArrayLike = Union[list[float], tuple[float, ...], FloatArray]
 FloatOrArray = Union[float, FloatArray]
 FloatOrArrayLike = Union[float, FloatArrayLike]
 
 FloatVector = NDArray[Shape['*'], Float64]
-FloatVectorLike = Union[list[float], FloatVector]
+FloatVectorLike = Union[list[float], tuple[float, ...], FloatVector]
 FloatOrVector = Union[float, FloatVector]
 FloatOrVectorLike = Union[float, FloatVectorLike]
 
