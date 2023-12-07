6faefe65d2f6ef6eeacb9c8ba35a922b18e2efb2
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2023-12-07 23:19:47 +0100

    increase rtol to avoid false negatives

--- tests/distributions/test_distributions.py
+++ tests/distributions/test_distributions.py
@@ -152,7 +152,7 @@ def test_random(dist1, dist2):
     DPn = 49
     PDI = 1.8
     num_samples = 10**6
-    rtol = [None, 5e-3, 2e-2, 5e-2]
+    rtol = [None, 5e-3, 2e-2, 10e-2]
     for d in distributions:
         d.DPn = DPn
         if isinstance(d, (LogNormal, SchulzZimm)):
