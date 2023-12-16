e127122a1fd1fe85f29dfefb03da5058dd2fd9ef
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2023-12-16 18:52:55 +0100

    refactor

--- src/polykin/properties/__init__.py
+++ src/polykin/properties/__init__.py
@@ -7,6 +7,6 @@ This module implements methods to evaluate physical property equations and
 estimate physical properties often used in polymer reactor models.
 """
 
-from .diffusion import *
 from .equations import *
 from .pvt_polymer import *
+from .mixing_rules import *
