3143f7bccb8f77bc21c4a1bf0200fd65f49c8c35
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2023-12-18 21:05:12 +0100

    add geometric_interaction_mixing, refactor imports, V>v

--- src/polykin/properties/mixing_rules.py
+++ src/polykin/properties/mixing_rules.py
@@ -5,19 +5,29 @@
 from polykin.types import FloatVector, FloatSquareMatrix
 
 import numpy as np
+from numpy import sqrt, dot
 from scipy.constants import R
 from typing import Optional
 
-__all__ = ['quadratic_mixing_rule',
+__all__ = ['quadratic_mixing',
+           'geometric_interaction_mixing',
            'pseudocritical_properties']
 
 # %% Mixing rules
 
 
-def quadratic_mixing_rule(y: FloatVector, Q: FloatSquareMatrix) -> float:
-    r"""Calculate mixture parameter using quadratic mixing rule.
+def quadratic_mixing(y: FloatVector,
+                     Q: FloatSquareMatrix
+                     ) -> float:
+    r"""Calculate a mixture parameter using a quadratic mixing rule.
 
-    $$ Q_m = sum_i sum_j y_i y_j Q_{ij} $$
+    $$ Q_m = \sum_i \sum_j y_i y_j Q_{ij} $$
+
+    !!! note
+
+        For the sake of generality, no assumptions are made regarding the
+        symmetry of $Q_{ij}$. The _full_ matrix must be supplied and will be
+        used as given.
 
     Reference:
 
@@ -36,17 +46,53 @@ def quadratic_mixing_rule(y: FloatVector, Q: FloatSquareMatrix) -> float:
     float
         Mixture parameter, $Q_m$. Unit = [Q].
     """
-    # alternative loop implementation, if Q is symmetric
-    # N = y.size
-    # Qm = 0.
-    # for i in range(N):
-    #     Qm += y[i]**2 * Q[i, i]
-    # for i in range(N):
-    #     for j in range(i+1, N):
-    #         Qm += 2*y[i]*y[j]*Q[i, j]
-    Qm = np.sum(np.outer(y, y)*Q, dtype=np.float64)
-    return Qm
+    return np.sum(np.outer(y, y)*Q, dtype=np.float64)
 
+
+def geometric_interaction_mixing(y: FloatVector,
+                                 Q: FloatVector,
+                                 k: Optional[FloatSquareMatrix] = None
+                                 ) -> float:
+    r"""Calculate a mixture parameter using a geometric average with
+    interaction.
+
+    $$ Q_m = \sum_i \sum_j y_i y_j (Q_i Q_j)^{1/2}(1-k_{ij}) $$
+
+    with $k_{ii}=0$ and $k_{i,j}=k_{j,i}$.
+
+    !!! note
+
+        Only the entries above the main diagonal of $k_{i,j}$ are used.
+
+    Reference:
+
+    * RC Reid, JM Prausniz, and BE Poling. The properties of gases & liquids
+    4th edition, 1986, p. 75.
+
+    Parameters
+    ----------
+    y : FloatVector
+        Composition, usually molar or mass fractions.
+    Q : FloatVector
+        Pure component property.
+    k : FloatSquareMatrix | None
+        Binary interaction parameter matrix.
+
+    Returns
+    -------
+    float
+        Mixture parameter, $Q_m$. Unit = [Q].
+    """
+    if k is None:
+        Qm = (dot(y, sqrt(Q)))**2
+    else:
+        N = y.size
+        Qm = 0.
+        for i in range(N):
+            Qm += y[i]**2 * Q[i]
+            for j in range(i+1, N):
+                Qm += 2*y[i]*y[j]*sqrt(Q[i]*Q[j])*(1. - k[i, j])
+    return Qm
 
 # %% Pseudocritical properties
 
@@ -63,8 +109,8 @@ def pseudocritical_properties(y: FloatVector,
     $$ \begin{aligned}
         T_{cm} &= \sum_i y_i T_{ci} \\
         Z_{cm} &= \sum_i y_i Z_{ci} \\
-        V_{cm} &= \sum_i y_i \frac{Z_{ci} R T_{ci}}{P_{ci}} \\
-        P_{cm} &= \frac{Z_{cm} R T_{cm}}{V_{cm}} \\
+        v_{cm} &= \sum_i y_i \frac{Z_{ci} R T_{ci}}{P_{ci}} \\
+        P_{cm} &= \frac{Z_{cm} R T_{cm}}{v_{cm}} \\
         \omega_{cm} &= \sum_i y_i \omega_{ci}
     \end{aligned} $$
 
@@ -92,18 +138,18 @@ def pseudocritical_properties(y: FloatVector,
     -------
     tuple[float, float, float, float, float]
         Tuple of pseudocritial properties,
-        $(T_{cm}, P_{cm}, V_{cm}, Z_{cm}, \omega_{cm})$.
+        $(T_{cm}, P_{cm}, v_{cm}, Z_{cm}, \omega_{cm})$.
     """
 
-    Tc_mix = np.dot(y, Tc)
-    Zc_mix = np.dot(y, Zc)
-    Vc = Zc*R*Tc/Pc
-    Vc_mix = np.dot(y, Vc)
-    Pc_mix = R*Zc_mix*Tc_mix/Vc_mix
+    Tc_mix = dot(y, Tc)
+    Zc_mix = dot(y, Zc)
+    vc = Zc*R*Tc/Pc
+    vc_mix = dot(y, vc)
+    Pc_mix = R*Zc_mix*Tc_mix/vc_mix
 
     if w is not None:
-        w_mix = np.dot(y, w)
+        w_mix = dot(y, w)
     else:
         w_mix = -1e99
 
-    return (Tc_mix, Pc_mix, Vc_mix, Zc_mix, w_mix)
+    return (Tc_mix, Pc_mix, vc_mix, Zc_mix, w_mix)
