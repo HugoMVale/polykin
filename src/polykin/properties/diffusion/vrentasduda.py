0e5f9d3fb7b8b4b57523473bd45bb58d2f636248
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2024-01-08 21:16:32 +0100

    move examples to docstrings

--- src/polykin/properties/diffusion/vrentasduda.py
+++ src/polykin/properties/diffusion/vrentasduda.py
@@ -98,6 +98,27 @@ class VrentasDudaBinary():
         Unit of diffusivity, by definition equal to L²/T.
     name : str
         Name.
+
+    Examples
+    --------
+    Estimate the mutual and self-diffusion coefficient of toluene in
+    polyvinylacetate at 20 wt% toluene and 25°C.
+
+    >>> from polykin.properties.diffusion import VrentasDudaBinary
+    >>> d = VrentasDudaBinary(
+    ...     D0=4.82e-4, E=0., V1star=0.917, V2star=0.728, z=0.82,
+    ...     K11=1.45e-3, K12=4.33e-4, K21=-86.32, K22=-258.2, X=0.5,
+    ...     unit='cm²/s',
+    ...     name='Tol(1)/PVAc(2)')
+
+    >>> D = d(0.2, 25., Tunit='C')
+    >>> print(f"D = {D:.2e} {d.unit}")
+    D = 3.79e-08 cm²/s
+
+    >>> D1 = d(0.2, 25., Tunit='C', selfd=True)
+    >>> print(f"D1 = {D1:.2e} {d.unit}")
+    D1 = 7.40e-08 cm²/s
+
     """  # noqa: E501
 
     D0: float
