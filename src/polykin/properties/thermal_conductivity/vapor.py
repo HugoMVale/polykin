aa0ec5aed49d21a7d487d587dbe68b589588c03e
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2024-02-11 21:11:22 +0100

    change arrays to list

--- src/polykin/properties/thermal_conductivity/vapor.py
+++ src/polykin/properties/thermal_conductivity/vapor.py
@@ -227,11 +227,10 @@ def KVMX2_Wassilijewa(y: FloatVectorLike,
     Estimate the thermal conductivity of a 50 mol% styrene/ethyl-benzene gas
     mixture at 25°C and 0.1 bar.
     >>> from polykin.properties.thermal_conductivity import KVMX2_Wassilijewa
-    >>> import numpy as np
     >>>
-    >>> y = np.array([0.5, 0.5])
-    >>> k = np.array([1.00e-2, 1.55e-2]) # W/(m·K), from literature
-    >>> M = np.array([104.15, 106.17])   # g/mol
+    >>> y = [0.5, 0.5]
+    >>> k = [1.00e-2, 1.55e-2] # W/(m·K), from literature
+    >>> M = [104.15, 106.17]   # g/mol
     >>>
     >>> k_mix = KVMX2_Wassilijewa(y, k, M)
     >>>
