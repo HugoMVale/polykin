69dc9d3cd76c74f48dd0be687204ad1cdcf34a4b
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2024-03-22 22:50:36 +0100

    add FH and DGmix-a consistency test

--- tests/thermo/test_acm.py
+++ tests/thermo/test_acm.py
@@ -4,13 +4,14 @@
 
 import numpy as np
 from numpy import isclose
+from scipy.constants import gas_constant as R
 
-from polykin.thermo.acm import (NRTL, UNIQUAC, FloryHuggins2_a, FloryHuggins_a,
-                                IdealSolution)
+from polykin.thermo.acm import (NRTL, UNIQUAC, FloryHuggins, FloryHuggins2_a,
+                                FloryHuggins_a, IdealSolution)
 
 
 def test_IdealSolution():
-    m = IdealSolution()
+    m = IdealSolution(2)
     T = 298.
     x = np.array([0.5, 0.5])
     assert isclose(m.gE(T, x), 0.)
@@ -34,6 +35,11 @@ def test_NRTL():
     assert np.all(isclose(m.gamma(T, np.array([1., 0.])),
                           [1., 4.557085], rtol=1e-6))
     assert isclose(m.Dgmix(T, np.array([0.5, 0.5])), -0.98183e3, rtol=1e-4)
+    # activity formula <> DGmix
+    x = np.array([0.3, 0.7])
+    Dgmix = m.Dgmix(T, x)
+    a = m.a(T, x)
+    assert isclose(Dgmix, R*T*np.sum(x*np.log(a)))
 
 
 def test_UNIQUAC():
@@ -46,6 +52,10 @@ def test_UNIQUAC():
     x = np.array([.1311, .0330, .8359])
     T = 45 + 273.15
     assert np.all(isclose(m.gamma(T, x), [7.15, 1.25, 1.06], rtol=2e-3))
+    # activity formula <> DGmix
+    Dgmix = m.Dgmix(T, x)
+    a = m.a(T, x)
+    assert isclose(Dgmix, R*T*np.sum(x*np.log(a)))
 
 
 def test_UNIQUAC_2():
@@ -96,3 +106,33 @@ def test_FloryHuggins_a():
                         np.array([1, 1, m]),
                         np.array([[0, 0, chi], [0, 0, chi], [chi, chi, 0]]))
     assert np.all(isclose(a2, [a3[0:2].sum(), a3[-1]]))
+
+
+def test_FloryHuggins():
+    N = 2
+    T = 298.
+    phi1 = 0.3
+    phi = np.array([phi1, 1-phi1])
+    # ideal solution
+    X = np.zeros((N, N))
+    m = np.array([1, 1])
+    fh = FloryHuggins(N, a=X)
+    id = IdealSolution(N)
+    assert isclose(fh.Dhmix(T, phi, m), 0.)
+    assert isclose(fh.Dsmix(T, phi, m), id.Dsmix(T, phi))
+    assert isclose(fh.Dgmix(T, phi, m), id.Dgmix(T, phi))
+    # regular solution (DSmix=0, DHmix>0)
+    chi = 0.4
+    X = np.array([[0, chi], [chi, 0]])
+    m = np.array([1, 1])
+    fh = FloryHuggins(N, b=X*T)
+    assert fh.Dhmix(T, phi, m) > 0
+    assert isclose(fh.Dsmix(T, phi, m), id.Dsmix(T, phi))
+    # activity formula <> DGmix
+    chi = 0.3
+    X = np.array([[0, chi], [chi, 0]])
+    m = np.array([1, 13])
+    fh = FloryHuggins(N, b=X*T)
+    Dgmix = fh.Dgmix(T, phi, m)
+    a = fh.a(T, phi, m)
+    assert isclose(Dgmix, R*T*np.sum(phi/m*np.log(a)))
