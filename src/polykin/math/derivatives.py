8c82d7a9da561f9f86d86443e2ca92c44d43aab9
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2024-04-21 17:00:20 +0200

    add examples

--- src/polykin/math/derivatives.py
+++ src/polykin/math/derivatives.py
@@ -27,7 +27,7 @@ def derivative_complex(f: Callable[[complex], complex],
         evaluated with complex inputs, but which per se do not implement
         complex arithmetic.
 
-    **Reference**
+    **References**
 
     *   J. Martins and A. Ning. Engineering Design Optimization. Cambridge
     University Press, 2021.
@@ -44,6 +44,14 @@ def derivative_complex(f: Callable[[complex], complex],
     -------
     tuple[float, float]
         Tuple with derivative and function value, $(f'(x), f(x))$.
+
+    Examples
+    --------
+    Evaluate the numerical derivative of f(x)=x**3 at x=1.
+    >>> from polykin.math import derivative_complex
+    >>> def f(x): return x**3
+    >>> derivative_complex(f, 1.)
+    (3.0, 1.0)
     """
     fz = f(complex(x, eps))
     return (fz.imag/eps, fz.real)
@@ -57,7 +65,7 @@ def derivative_centered(f: Callable[[float], float],
 
     $$ f'(x) = \frac{f(x + h) - f(x - h)}{2 h} + O(h^2) $$
 
-    **Reference**
+    **References**
 
     *   J. Martins and A. Ning. Engineering Design Optimization. Cambridge
     University Press, 2021.
@@ -74,6 +82,14 @@ def derivative_centered(f: Callable[[float], float],
     -------
     tuple[float, float]
         Tuple with derivative and mean function value, $(f'(x), f(x))$.
+
+    Examples
+    --------
+    Evaluate the numerical derivative of f(x)=x**3 at x=1.
+    >>> from polykin.math import derivative_centered
+    >>> def f(x): return x**3
+    >>> derivative_centered(f, 1.)
+    (3.000000000069477, 1.0000000002288205)
     """
     h = cbrt(3*eps)
     fp = f(x + h)
