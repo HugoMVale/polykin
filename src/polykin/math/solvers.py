ab80b18e1ac1d66f2f10f7da9ad74c95820d25ac
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2024-08-27 21:36:22 +0200

    extend ode_rk to orders 1,2, 4

--- src/polykin/math/solvers.py
+++ src/polykin/math/solvers.py
@@ -4,13 +4,13 @@
 
 import math
 from dataclasses import dataclass
-from typing import Callable
+from typing import Callable, Literal
 
 from numba import njit
 
 from polykin.math.derivatives import derivative_complex
 
-__all__ = ['root_newton', 'root_secant', 'ode_rk2']
+__all__ = ['root_newton', 'root_secant', 'ode_rk']
 
 
 @dataclass
@@ -171,13 +171,14 @@ def root_secant(f: Callable[[float], float],
 
 
 @njit
-def ode_rk2(t0: float,
-            tf: float,
-            y0: float,
-            h: float,
-            f: Callable[[float, float], float]
-            ) -> float:
-    r"""Integrate an ODE using the 2nd-order Runge-Kutta mid-point method.
+def ode_rk(f: Callable[[float, float], float],
+           t0: float,
+           tf: float,
+           y0: float,
+           h: float,
+           order: Literal[1, 2, 4] = 1
+           ) -> float:
+    r"""Integrate an ODE using a fixed-step Runge-Kutta scheme.
 
     This method is intentionally simple, so that it can be used inside a
     gradient-based optimizer without creating numerical noise and overhead.
@@ -189,6 +190,9 @@ def ode_rk2(t0: float,
 
     Parameters
     ----------
+    f : Callable[[float, float], float]
+        Function to be integrated. Takes two arguments, `t` and `y`, and returns
+        the derivative of `y` with respect to `t`.
     t0 : float
         Initial value of `t`.
     tf : float
@@ -197,9 +201,8 @@ def ode_rk2(t0: float,
         Initial value of `y`, i.e., `y(t0)`.
     h : float
         Step size.
-    f : Callable[[float, float], float]
-        Function to be integrated. Takes two arguments, `t` and `y`, and returns
-        the derivative of `y` with respect to `t`.
+    order : Literal[1, 2, 4]
+        Order of the method. Defaults to 1 (i.e., Euler).
 
     Returns
     -------
@@ -210,27 +213,57 @@ def ode_rk2(t0: float,
     --------
     Find the solution of the differential equation $y(t)'=y+t$ with initial
     condition $y(0)=1$ at $t=2$.
-    >>> from polykin.math import ode_rk2
+    >>> from polykin.math import ode_rk
     >>> from numba import njit
     >>> def ydot(t, y):
     ...     return y + t
-    >>> ode_rk2(0., 2., 1., 1e-3, njit(ydot))
+    >>> ode_rk(njit(ydot), 0., 2., 1., 1e-3, order=1)
+    11.763351307112204
+    >>> ode_rk(njit(ydot), 0., 2., 1., 1e-3, order=2)
     11.778107275517668
+    >>> ode_rk(njit(ydot), 0., 2., 1., 1e-3, order=4)
+    11.778112197860988
     """
 
-    def rk_step(t, y, h):
+    def step_rk1(f, t, y, h):
+        "Explicit 1st-order (Euler) step."
+        y += f(t, y) * h
+        return y
+
+    def step_rk2(f, t, y, h):
         "Explicit 2nd-order mid-point step."
-        return y + f(t + h / 2, y + f(t, y) * h / 2) * h
+        y += f(t + h / 2, y + f(t, y) * h / 2) * h
+        return y
 
-    nsteps = math.floor((tf - t0)/h)
-    hf = (tf - t0) - nsteps*h
-    t = t0
-    y = y0
-    for _ in range(nsteps):
-        y = rk_step(t, y, h)
-        t += h
+    def step_rk4(f, t, y, h):
+        "Explicit 4th-order Runge-Kutta step."
+        k1 = f(t, y)
+        k2 = f(t + h / 2, y + k1 * h / 2)
+        k3 = f(t + h / 2, y + k2 * h / 2)
+        k4 = f(t + h, y + k3 * h)
+        y += (k1 + 2 * k2 + 2 * k3 + k4) * h / 6
+        return y
 
-    y = rk_step(t, y, hf)
-    t += hf
+    def integrate(step, f, t0, tf, y0, h):
+        "Integrate using the given step function."
+        nsteps = math.floor((tf - t0)/h)
+        hf = (tf - t0) - nsteps*h
+        t = t0
+        y = y0
+        for _ in range(nsteps):
+            y = step(f, t, y, h)
+            t += h
+        y = step(f, t, y, hf)
+        t += hf
+        return y
 
-    return y
+    # Needs to be done in this explict/verbose form, because function pointers
+    # are not supported in numba.
+    if order == 1:
+        return integrate(step_rk1, f, t0, tf, y0, h)
+    elif order == 2:
+        return integrate(step_rk2, f, t0, tf, y0, h)
+    elif order == 4:
+        return integrate(step_rk4, f, t0, tf, y0, h)
+    else:
+        raise ValueError("Order must be 1, 2, or 4.")
--- tests/math/test_solvers.py
+++ tests/math/test_solvers.py
@@ -3,11 +3,10 @@
 # Copyright Hugo Vale 2024
 from math import exp
 
+from numba import njit
 from numpy import isclose
 
-from polykin.math.solvers import ode_rk2, root_newton, root_secant
-
-from numba import njit
+from polykin.math.solvers import ode_rk, root_newton, root_secant
 
 
 def f(x):
@@ -50,19 +49,21 @@ def test_root_secant():
     assert abs(res.f) < ftol
 
 
-def test_ode_rk2():
-    @njit
-    def ydot(t, y):
-        return y + t**2
+def test_ode_rk():
 
     t0 = 0.
     y0 = 1.
     tf = 4.
-    h = 1e-2
-    yf = ode_rk2(t0, tf, y0, h, ydot)
+
+    @njit
+    def ydot(t, y):
+        return y + t**2
 
     def ysol(t):
+        "Exact analytical solution."
         c = 3.
         return c*exp(t) - t**2 - 2*t - 2
 
-    assert isclose(yf, ysol(tf), rtol=1e-4)
+    for h, order in zip([1e-5, 1e-2, 1e-1], [1, 2, 4]):
+        yf = ode_rk(ydot, t0, tf, y0, h, order)  # type: ignore
+        assert isclose(yf, ysol(tf), rtol=1e-4)
