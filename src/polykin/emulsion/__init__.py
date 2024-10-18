ec48f60710f47381e711182af49751b05d16fa07
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2024-10-18 19:08:31 +0200

    add nbar formulas

---
+++ src/polykin/emulsion/__init__.py
@@ -0,0 +1,9 @@
+# PolyKin: A polymerization kinetics library for Python.
+#
+# Copyright Hugo Vale 2024
+
+"""
+This module implements methods for emulsion polymerization systems.
+"""
+
+from .nbar import *
