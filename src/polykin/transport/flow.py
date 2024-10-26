24a2c37250171184289d6ee40def615a076ac623
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2024-10-26 19:23:33 +0200

    simplify import in examples

--- src/polykin/transport/flow.py
+++ src/polykin/transport/flow.py
@@ -53,7 +53,7 @@ def DP_Hagen_Poiseuille(Q: float,
     --------
     Calculate the pressure drop for a polymer solution with a viscosity of
     10 Pa.s flowing through 5 m of DN50 tube at 1 L/s. 
-    >>> from polykin.transport.flow import DP_Hagen_Poiseuille
+    >>> from polykin.transport import DP_Hagen_Poiseuille
     >>> from math import pi
     >>> Q = 1e-3  # m³/s
     >>> D = 50e-3 # m
@@ -122,7 +122,7 @@ def DP_Darcy_Weisbach(v: float,
     --------
     Calculate the pressure drop for water flowing through 500 m of DN25 PVC
     pipe at 2 m/s.
-    >>> from polykin.transport.flow import DP_Darcy_Weisbach, fD_Haaland
+    >>> from polykin.transport import DP_Darcy_Weisbach, fD_Haaland
     >>> rho = 1e3 # kg/m³
     >>> mu = 1e-3 # Pa·s
     >>> L = 5e2   # m
@@ -191,6 +191,67 @@ def DP_tube(Q: float,
     return DP_Darcy_Weisbach(v, D, L, rho, fD)
 
 
+def DP_packed_bed(G: float,
+                  L: float,
+                  Dp: float,
+                  eps: float,
+                  rho: float,
+                  mu: float
+                  ) -> float:
+    r"""Calculate the pressure drop in a packed bed.
+
+    The pressure drop in a packed bed is given by:
+
+    $$ \Delta P = \frac{G^2 (1 - \epsilon) f_p}{\rho D_p \epsilon^3} L $$
+
+    where $G$ is the mass flux, $D_p$ is the particle diameter, $\epsilon$ is
+    the bed porosity, $L$ is the packed bed length, and $\rho$ is the fluid
+    density. The packing friction factor $f_p$ is estimated using the Sato and
+    Tallmadge correlation:
+
+    $$ f_p = \frac{150}{\mathrm{Re}_p} + \frac{4.2}{\mathrm{Re}_p^{1/6}} $$
+
+    where $\mathrm{Re}_p=D_p G/(\mu (1-\epsilon))$ is the packing Reynolds
+    number.
+
+    **References**
+
+    *   Walas, S. M. (1988). Chemical Process Equipment: Selection and Design.
+        Singapore: Butterworths.
+
+    Parameters
+    ----------
+    G : float
+        Mass flux (kg/m²·s).
+    L : float
+        Packed bed length (m).
+    Dp : float
+        Particle diameter (m).
+    eps : float
+        Bed porosity.
+    rho : float
+        Fluid density (kg/m³).
+    mu : float
+        Fluid viscosity (Pa·s).
+
+    Returns
+    -------
+    float
+        Pressure drop (Pa).
+
+    Examples
+    --------
+    Calculate the pressure drop in a packed bed reactor.
+    >>> from polykin.transport import DP_packed_bed
+    >>> DP = DP_packed_bed(G=50., L=2., Dp=1e-2, eps=0.45, rho=800., mu=0.01)
+    >>> print(f"DP = {DP:.1e} Pa")
+    DP = 1.4e+04 Pa
+    """
+    Rep = Dp*G/(mu*(1 - eps))
+    fp = 150/Rep + 4.2/Rep**(1/6)
+    return G**2*(1 - eps)*fp*L/(rho*Dp*eps**3)
+
+
 def fD_Colebrook(Re: float, er: float) -> float:
     r"""Calculate the Darcy friction factor using Colebrook's equation.
 
@@ -229,7 +290,7 @@ def fD_Colebrook(Re: float, er: float) -> float:
     --------
     Calculate the friction factor for water flowing through a DN25 PVC pipe at
     2 m/s.
-    >>> from polykin.transport.flow import fD_Colebrook
+    >>> from polykin.transport import fD_Colebrook
     >>> Re = 1e3*2*25e-3/1e-3
     >>> er = 0.0015/25
     >>> fD = fD_Colebrook(Re, er)
@@ -283,7 +344,7 @@ def fD_Haaland(Re: float, er: float) -> float:
     --------
     Calculate the friction factor for water flowing through a DN25 PVC pipe at
     2 m/s.
-    >>> from polykin.transport.flow import fD_Haaland
+    >>> from polykin.transport import fD_Haaland
     >>> Re = 1e3*2*25e-3/1e-3
     >>> er = 0.0015/25
     >>> fD = fD_Haaland(Re, er)
@@ -327,7 +388,7 @@ def Cd_sphere(Re: float) -> float:
     Examples
     --------
     Calculate the drag coefficient for a tennis ball traveling at 30 m/s.
-    >>> from polykin.transport.flow import Cd_sphere
+    >>> from polykin.transport import Cd_sphere
     >>> D = 6.7e-2  # m
     >>> mu = 1.6e-5 # Pa·s
     >>> rho = 1.2   # kg/m³
@@ -376,7 +437,7 @@ def terminal_velocity_Stokes(D: float,
     Examples
     --------
     Calculate the terminal velocity of a 500 nm PVC particle in water.
-    >>> from polykin.transport.flow import terminal_velocity_Stokes
+    >>> from polykin.transport import terminal_velocity_Stokes
     >>> vt = terminal_velocity_Stokes(500e-9, 1.4e3, 1e3, 1e-3)
     >>> print(f"vt = {vt:.1e} m/s")
     vt = 5.4e-08 m/s
@@ -436,7 +497,7 @@ def terminal_velocity_sphere(D: float,
     Examples
     --------
     Calculate the terminal velocity of a 1 mm styrene droplet in air.
-    >>> from polykin.transport.flow import terminal_velocity_sphere
+    >>> from polykin.transport import terminal_velocity_sphere
     >>> vt = terminal_velocity_sphere(1e-3, 910., 1.2, 1.6e-5)
     >>> print(f"vt = {vt:.1f} m/s")
     vt = 3.8 m/s
@@ -450,64 +511,3 @@ def terminal_velocity_sphere(D: float,
     sol = root_newton(fnc, terminal_velocity_Stokes(D, rhop, rho, mu))
 
     return sol.x
-
-
-def DP_packed_bed(G: float,
-                  L: float,
-                  Dp: float,
-                  eps: float,
-                  rho: float,
-                  mu: float
-                  ) -> float:
-    r"""Calculate the pressure drop in a packed bed.
-
-    The pressure drop in a packed bed is given by:
-
-    $$ \Delta P = \frac{G^2 (1 - \epsilon) f_p}{\rho D_p \epsilon^3} L $$
-
-    where $G$ is the mass flux, $D_p$ is the particle diameter, $\epsilon$ is
-    the bed porosity, $L$ is the packed bed length, and $\rho$ is the fluid
-    density. The packing friction factor $f_p$ is estimated using the Sato and
-    Tallmadge correlation:
-
-    $$ f_p = \frac{150}{\mathrm{Re}_p} + \frac{4.2}{\mathrm{Re}_p^{1/6}} $$
-
-    where $\mathrm{Re}_p=D_p G/(\mu (1-\epsilon))$ is the packing Reynolds
-    number.
-
-    **References**
-
-    *   Walas, S. M. (1988). Chemical Process Equipment: Selection and Design.
-        Singapore: Butterworths.
-
-    Parameters
-    ----------
-    G : float
-        Mass flux (kg/m²·s).
-    L : float
-        Packed bed length (m).
-    Dp : float
-        Particle diameter (m).
-    eps : float
-        Bed porosity.
-    rho : float
-        Fluid density (kg/m³).
-    mu : float
-        Fluid viscosity (Pa·s).
-
-    Returns
-    -------
-    float
-        Pressure drop (Pa).
-
-    Examples
-    --------
-    Calculate the pressure drop in a packed bed reactor.
-    >>> from polykin.transport.flow import DP_packed_bed
-    >>> DP = DP_packed_bed(G=50., L=2., Dp=1e-2, eps=0.45, rho=800., mu=0.01)
-    >>> print(f"DP = {DP:.1e} Pa")
-    DP = 1.4e+04 Pa
-    """
-    Rep = Dp*G/(mu*(1 - eps))
-    fp = 150/Rep + 4.2/Rep**(1/6)
-    return G**2*(1 - eps)*fp*L/(rho*Dp*eps**3)
