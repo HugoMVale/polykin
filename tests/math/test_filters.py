5f767b9bd9b60cc8a1d3996e49b60e20be085e1b
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2024-11-16 17:00:36 +0100

    extend tests

--- tests/math/test_filters.py
+++ tests/math/test_filters.py
@@ -3,6 +3,7 @@
 # Copyright Hugo Vale 2024
 
 import numpy as np
+import pytest
 from numpy import all, isclose
 
 from polykin.math.filters import simplify_polyline
@@ -22,3 +23,11 @@ def test_polyline():
     res = simplify_polyline(p, tol=1.0)
     sol = np.array([(0, 0), (2, -0.1), (3, 5), (7, 9), (9, 9)])
     assert all(isclose(res, sol))
+
+    assert all(isclose(simplify_polyline(p[:2, :], tol=1.0), p[:2, :]))
+
+    with pytest.raises(ValueError):
+        _ = simplify_polyline(p[:, 0], tol=1.0)
+
+    with pytest.raises(ValueError):
+        _ = simplify_polyline(p[0, :], tol=1.0)
--- tests/math/test_solvers.py
+++ tests/math/test_solvers.py
@@ -3,6 +3,7 @@
 # Copyright Hugo Vale 2024
 from math import exp
 
+import pytest
 from numba import njit
 from numpy import isclose
 
@@ -67,3 +68,6 @@ def test_ode_rk():
     for h, order in zip([1e-5, 1e-2, 1e-1], [1, 2, 4]):
         yf = ode_rk(ydot, t0, tf, y0, h, order)  # type: ignore
         assert isclose(yf, ysol(tf), rtol=1e-4)
+
+    with pytest.raises(ValueError):
+        _ = ode_rk(ydot, 0., 1., 1., 1e-5, 5)
