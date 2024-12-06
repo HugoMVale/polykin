05486dc317f941fc6006f6dc032995ae139c4f65
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2024-12-06 23:02:37 +0100

    restrcture kinetics

--- src/polykin/kinetics/__init__.py
+++ src/polykin/kinetics/__init__.py
@@ -1,13 +1,7 @@
 # PolyKin: A polymerization kinetics library for Python.
 #
-# Copyright Hugo Vale 2023
+# Copyright Hugo Vale 2024
 
-"""
-This module implements methods to create and visualize the types of kinetic
-coefficients most often used in polymer reactor models.
-"""
-
-from .arrhenius import *
-from .cldpropagation import *
-from .cldtermination import *
-from .eyring import *
+from .coefficients import *
+from .emulsion import *
+from .rtd import *
