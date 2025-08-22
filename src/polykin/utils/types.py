dab5857dd964edf5231fdd1a73696a602bb7843d
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2025-08-22 23:28:09 +0200

    remove nptyping to allow numpy 2

--- src/polykin/utils/types.py
+++ src/polykin/utils/types.py
@@ -3,34 +3,32 @@
 # Copyright Hugo Vale 2023
 
 
-from typing import Any, TypeVar
+from typing import TypeVar
 
 import numpy as np
-from nptyping import Float64, Int32, NDArray, Shape
+from numpy.typing import NDArray
 
 Number = TypeVar("Number", float, complex)
-Floaty = TypeVar("Floaty", float, np.ndarray[Any, np.dtype[np.float64]])
+Floaty = TypeVar("Floaty", float, NDArray[np.float64])
 
-IntArray = np.ndarray[Any, np.dtype[np.int32]]
+IntArray = NDArray[np.int_]
 IntArrayLike = list[int] | tuple[int, ...] | IntArray
 
-IntVector = NDArray[Shape['*'], Int32]
+IntVector = NDArray[np.int_]
 IntVectorLike = list[int] | tuple[int, ...] | IntVector
 
-FloatArray = np.ndarray[Any, np.dtype[np.float64]]
+FloatArray = NDArray[np.float64]
 FloatArrayLike = list[float] | tuple[float, ...] | FloatArray
 FloatOrArray = float | FloatArray
 FloatOrArrayLike = float | FloatArrayLike
 
-FloatVector = NDArray[Shape['*'], Float64]
+FloatVector = NDArray[np.float64]
 FloatVectorLike = list[float] | tuple[float, ...] | FloatVector
 FloatOrVector = float | FloatVector
 FloatOrVectorLike = float | FloatVectorLike
 
-FloatMatrix = NDArray[Shape['*, *'], Float64]
-FloatSquareMatrix = NDArray[Shape['Dim, Dim'], Float64]
-Float2x2Matrix = NDArray[Shape['2, 2'], Float64]
+FloatMatrix = NDArray[np.float64]
+FloatSquareMatrix = NDArray[np.float64]
+Float2x2Matrix = NDArray[np.float64]
 
-FloatRangeArray = NDArray[Shape['2'], Float64]
-
-Floaty = TypeVar("Floaty", float, np.ndarray[Any, np.dtype[np.float64]])
+FloatRangeArray = NDArray[np.float64]
