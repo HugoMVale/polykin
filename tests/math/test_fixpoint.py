39fa060304891b46ac3600e62fe151a9dd4e8f84
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2025-11-15 21:09:26 +0100

    rename g.xs

--- tests/math/test_fixpoint.py
+++ tests/math/test_fixpoint.py
@@ -14,46 +14,46 @@ def g(x):
     return np.array([0.5*np.cos(x1) + 0.1*x2 + 0.5, np.sin(x2) - 0.2*x1 + 1.2])
 
 
-g.sol = np.array([0.97458605, 1.93830731])
+g.xs = np.array([0.97458605, 1.93830731])
 
 
 def test_fixpoint_anderson():
     # stop tolx
     tolx = 1e-13
     for m in range(1, 5):
-        res = fixpoint_anderson(g, np.array([0.0, 0.0]), m=m, tolx=tolx)
-        assert res.success
-        assert "tolx" in res.message
-        assert allclose(res.x, g.sol, atol=tolx)
-        assert allclose(res.f, g(res.x) - res.x)
+        sol = fixpoint_anderson(g, np.array([0.0, 0.0]), m=m, tolx=tolx)
+        assert sol.success
+        assert "tolx" in sol.message
+        assert allclose(sol.x, g.xs, atol=tolx)
+        assert allclose(sol.f, g(sol.x) - sol.x)
     # stop maxiter
     maxiter = 3
-    res = fixpoint_anderson(g, np.array([0.0, 0.0]), maxiter=maxiter)
-    assert not res.success
-    assert "iterations" in res.message
-    assert res.niter == maxiter
-    assert allclose(res.f, g(res.x) - res.x)
+    sol = fixpoint_anderson(g, np.array([0.0, 0.0]), maxiter=maxiter)
+    assert not sol.success
+    assert "iterations" in sol.message
+    assert sol.niter == maxiter
+    assert allclose(sol.f, g(sol.x) - sol.x)
 
 
 def test_fixpoint_wegstein():
     # stop tolx, normal
     tolx = 1e-8
-    res = fixpoint_wegstein(g, np.array([0.0, 0.0]), qmax=0.5, tolx=tolx)
-    assert res.success
-    assert "tolx" in res.message
-    assert allclose(res.x, g.sol, atol=tolx)
-    assert allclose(res.f, g(res.x) - res.x)
+    sol = fixpoint_wegstein(g, np.array([0.0, 0.0]), qmax=0.5, tolx=tolx)
+    assert sol.success
+    assert "tolx" in sol.message
+    assert allclose(sol.x, g.xs, atol=tolx)
+    assert allclose(sol.f, g(sol.x) - sol.x)
     # stop tolx, no acceleration
     tolx = 1e-8
-    res = fixpoint_wegstein(g, np.array([0.0, 0.0]), wait=100, tolx=tolx)
-    assert res.success
-    assert "tolx" in res.message
-    assert allclose(res.x, g.sol, atol=tolx)
-    assert allclose(res.f, g(res.x) - res.x)
+    sol = fixpoint_wegstein(g, np.array([0.0, 0.0]), wait=100, tolx=tolx)
+    assert sol.success
+    assert "tolx" in sol.message
+    assert allclose(sol.x, g.xs, atol=tolx)
+    assert allclose(sol.f, g(sol.x) - sol.x)
     # stop maxiter
     maxiter = 3
-    res = fixpoint_wegstein(g, np.array([0.0, 0.0]), maxiter=maxiter)
-    assert not res.success
-    assert "iterations" in res.message
-    assert res.niter == maxiter
-    assert allclose(res.f, g(res.x) - res.x)
+    sol = fixpoint_wegstein(g, np.array([0.0, 0.0]), maxiter=maxiter)
+    assert not sol.success
+    assert "iterations" in sol.message
+    assert sol.niter == maxiter
+    assert allclose(sol.f, g(sol.x) - sol.x)
