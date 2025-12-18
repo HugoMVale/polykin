4235de1d2b3227b6da233581df74a1983a3413e3
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2025-12-18 22:34:46 +0100

    Refactor imports and formatting

--- src/polykin/kinetics/emulsion/nbar.py
+++ src/polykin/kinetics/emulsion/nbar.py
@@ -5,9 +5,10 @@
 from numpy import sqrt
 from scipy.special import iv
 
-__all__ = ['nbar_Stockmayer_OToole',
-           'nbar_Li_Brooks'
-           ]
+__all__ = [
+    "nbar_Stockmayer_OToole",
+    "nbar_Li_Brooks",
+]
 
 
 def nbar_Stockmayer_OToole(alpha: float, m: float) -> float:
@@ -36,18 +37,18 @@ def nbar_Stockmayer_OToole(alpha: float, m: float) -> float:
     float
         Average number of radicals per particle.
     """
-    a = sqrt(8*alpha)
-    return (a/4)*iv(m, a)/iv(m-1, a)
+    a = sqrt(8 * alpha)
+    return (a / 4) * iv(m, a) / iv(m - 1, a)
 
 
 def nbar_Li_Brooks(alpha: float, m: float) -> float:
     r"""Average number of radicals per particle according to the Li-Brooks
     approximation.
 
-    $$ \bar{n} = \frac{2 \alpha}{m + \sqrt{m^2 + 
+    $$ \bar{n} = \frac{2 \alpha}{m + \sqrt{m^2 +
         \frac{8 \alpha \left( 2 \alpha + m \right)}{2 \alpha + m + 1}}} $$
 
-    This formula agrees well with the exact Stockmayer-O'Toole solution, 
+    This formula agrees well with the exact Stockmayer-O'Toole solution,
     with a maximum deviation of about 4%.
 
     **References**
@@ -68,4 +69,6 @@ def nbar_Li_Brooks(alpha: float, m: float) -> float:
     float
         Average number of radicals per particle.
     """
-    return 2*alpha/(m + sqrt(m**2 + 8*alpha*(2*alpha + m)/(2*alpha + m + 1)))
+    return (
+        2 * alpha / (m + sqrt(m**2 + 8 * alpha * (2 * alpha + m) / (2 * alpha + m + 1)))
+    )
