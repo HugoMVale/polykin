4c5a598e009707149cb2661db147f90660cf5c03
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2026-04-20 22:04:13 +0200

    add VectorOptimumResult

--- src/polykin/math/optimization/results.py
+++ src/polykin/math/optimization/results.py
@@ -5,8 +5,12 @@
 from dataclasses import dataclass
 
 from polykin.utils.tools import colored_bool
+from polykin.utils.typing import FloatVector
 
-__all__ = ["OptimumResult"]
+__all__ = [
+    "OptimumResult",
+    "VectorOptimumResult",
+]
 
 
 @dataclass(frozen=True, slots=True)
@@ -37,6 +41,49 @@ class OptimumResult:
     nfeval: int
     niter: int
     x: float
+    f: float
+
+    def __repr__(self) -> str:
+        """Return a string representation of the optimum result."""
+        return (
+            f" method: {self.method}\n"
+            f"success: {colored_bool(self.success)}\n"
+            f"message: {self.message}\n"
+            f" nfeval: {self.nfeval}\n"
+            f"  niter: {self.niter}\n"
+            f"      x: {self.x}\n"
+            f"      f: {self.f}"
+        )
+
+
+@dataclass(frozen=True, slots=True)
+class VectorOptimumResult:
+    """Dataclass with the results of the optimization.
+
+    Attributes
+    ----------
+    method: str
+        Method used to find the optimum.
+    success: bool
+        If `True`, the optimum was found.
+    message: str
+        Description of the exit status.
+    nfeval: int
+        Number of function evaluations.
+    niter: int
+        Number of iterations.
+    x: FloatVector
+        Optimum value.
+    f: float
+        Function value at the optimum.
+    """
+
+    method: str
+    success: bool
+    message: str
+    nfeval: int
+    niter: int
+    x: FloatVector
     f: float
 
     def __repr__(self) -> str:
